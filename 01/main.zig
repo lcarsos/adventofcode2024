7259a7c89593267f28d0b67009f0a7273aa0f941
lcarsos <lcarsos@lcarsos.com>
2024-12-03 17:10:07 -0700

    so close to actually learning how to default

--- 01/main.zig
+++ 01/main.zig
@@ -3,7 +3,7 @@ const Allocator = std.mem.Allocator;
 const page_allocator = std.heap.page_allocator;
 const ArrayList = std.ArrayList;
 
-fn parseList(alloc: Allocator, input: []const u8) ![2]ArrayList(i32) {
+fn parseList(alloc: Allocator, input: []const u8) [2]ArrayList(i32) {
     var lines = std.mem.split(u8, input, "\n");
     var first = ArrayList(i32).init(alloc);
     defer first.deinit();
@@ -12,7 +12,10 @@ fn parseList(alloc: Allocator, input: []const u8) ![2]ArrayList(i32) {
 
     while (lines.next()) |line| {
         var strs = std.mem.split(u8, line, " ");
-        const match: i32 = try std.fmt.parseInt(i32, strs.next().?, 10);
+        const match: i32 = std.fmt.parseInt(i32, strs.next().?, 10) catch |err| blk: {
+            std.debug.print("how do you actually handle an error in this language?\n{any}\n", .{err});
+            break :blk 47;
+        };
         std.debug.print("look: {any}\n", .{match});
         first.append(match);
         second.append(try std.fmt.parseInt(i32, strs.next().?, 10));
@@ -25,9 +28,6 @@ pub fn main() void {
     const alloc = std.heap.page_allocator;
 
     const input: []const u8 = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3";
-    const parsed = parseList(alloc, input) catch |err| {
-        std.debug.print("how do you actually handle an error in this language?\n{any}\n", .{err});
-        return;
-    };
+    const parsed = parseList(alloc, input);
     std.debug.print("{}\n", .{parsed[0].items[0]});
 }
