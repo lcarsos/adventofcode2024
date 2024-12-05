da4b14289f62d76c37378cde2988f3d8f7aac2e2
lcarsos <lcarsos@lcarsos.com>
2024-12-05 09:10:49 -0700

    what a joke you can't call abs

--- 01/main.zig
+++ 01/main.zig
@@ -1,17 +1,14 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const page_allocator = std.heap.page_allocator;
 const ArrayList = std.ArrayList;
 
 fn parseList(alloc: Allocator, input: []const u8) [2]ArrayList(i32) {
     var lines = std.mem.split(u8, input, "\n");
     var first = ArrayList(i32).init(alloc);
-    defer first.deinit();
     var second = ArrayList(i32).init(alloc);
-    defer second.deinit();
 
     while (lines.next()) |line| {
-        var strs = std.mem.split(u8, line, " ");
+        var strs = std.mem.split(u8, line, "   ");
         const match: i32 = std.fmt.parseInt(i32, strs.next().?, 10) catch unreachable;
         std.debug.print("look: {any}\n", .{match});
         first.append(match) catch unreachable;
@@ -21,10 +18,22 @@ fn parseList(alloc: Allocator, input: []const u8) [2]ArrayList(i32) {
     return .{ first, second };
 }
 
-pub fn main() void {
-    const alloc = std.heap.page_allocator;
+pub fn main() !void {
+    var allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = allocator.allocator();
 
     const input: []const u8 = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3";
     const parsed = parseList(alloc, input);
-    std.debug.print("{}\n", .{parsed[0].items[0]});
+    std.debug.print("{any}\n", .{parsed[0].items});
+    std.mem.sort(i32, parsed[0].items, {}, std.sort.asc(i32));
+    std.mem.sort(i32, parsed[1].items, {}, std.sort.asc(i32));
+    std.debug.print("{any}\n", .{parsed[0].items});
+
+    const diff = try ArrayList(i32).initCapacity(alloc, parsed[0].items.len);
+    for (parsed[0].items, parsed[1].items, 0..) |x, y, i| {
+        std.debug.print("{any} {any} {any}\n", .{x, y, i});
+        diff.items[i] = @abs(y - x);
+
+    }
+    std.debug.print("{any}\n", .{diff.items});
 }
