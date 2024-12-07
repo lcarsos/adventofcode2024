28bcf4d9f12b67be17ee383a8aaf6bf06b00d649
lcarsos <lcarsos@lcarsos.com>
2024-12-06 18:02:04 -0700

    handle a newline terminated list

--- 01/main.zig
+++ 01/main.zig
@@ -9,31 +9,34 @@ fn parseList(alloc: Allocator, input: []const u8) [2]ArrayList(i32) {
 
     while (lines.next()) |line| {
         var strs = std.mem.split(u8, line, "   ");
-        const match: i32 = std.fmt.parseInt(i32, strs.next().?, 10) catch unreachable;
-        std.debug.print("look: {any}\n", .{match});
+        const match: i32 = std.fmt.parseInt(i32, strs.next().?, 10) catch return .{ first, second };
         first.append(match) catch unreachable;
         second.append(std.fmt.parseInt(i32, strs.next().?, 10) catch unreachable) catch unreachable;
     }
-
-    return .{ first, second };
+    unreachable;
+    //return .{ first, second };
 }
 
 pub fn main() !void {
     var allocator = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = allocator.allocator();
 
-    const input: []const u8 = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3";
+    const input: []const u8 = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3\n";
     const parsed = parseList(alloc, input);
-    std.debug.print("{any}\n", .{parsed[0].items});
     std.mem.sort(i32, parsed[0].items, {}, std.sort.asc(i32));
     std.mem.sort(i32, parsed[1].items, {}, std.sort.asc(i32));
-    std.debug.print("{any}\n", .{parsed[0].items});
 
-    const diff = try ArrayList(i32).initCapacity(alloc, parsed[0].items.len);
+    std.debug.print("{d} {d}\n", .{parsed[0].items.len, parsed[1].items.len});
+    var diff = try ArrayList(i32).initCapacity(alloc, parsed[0].items.len);
+    try diff.resize(parsed[0].items.len);
+    var sum: i32 = 0;
     for (parsed[0].items, parsed[1].items, 0..) |x, y, i| {
-        std.debug.print("{any} {any} {any}\n", .{x, y, i});
-        diff.items[i] = @abs(y - x);
-
+        const this_diff = y - x;
+        if (this_diff < 0) {
+            std.debug.print("found a negative at idx {d}\n", .{i});
+        }
+        diff.items[i] = this_diff;
+        sum += this_diff;
     }
-    std.debug.print("{any}\n", .{diff.items});
+    std.debug.print("{d}\n", .{sum});
 }
