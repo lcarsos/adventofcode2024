eae6170f80db955716765dc68136da22901fcf21
lcarsos <lcarsos@lcarsos.com>
2025-01-29 22:59:58 -0700

    katas for reader/writer and splitting

--- 02/partOne.zig
+++ 02/partOne.zig
@@ -1,21 +1,26 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
+const stdin = std.io.getStdIn();
 const dbgPrt = std.debug.print;
+const splitSequence = std.mem.splitSequence;
 
 pub fn main() !void  {
-    const arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
-    //const alloc = arena.allocator();
+    const alloc = arena.allocator();
 
-    var table: [4096:0]u8 = undefined;
-    const reader = std.io.getStdIn().reader();
+    var a = ArrayList(u8).init(alloc);
+    defer a.deinit();
+    var buffered_reader = std.io.bufferedReader(stdin.reader().any());
+    const buf_read = buffered_reader.reader();
 
-    while (try reader.read(&table)) |len| {
-        const slice = table[0..len-1];
-        dbgPrt("{s}\n", .{ slice, });
-    } else |err| {
-        switch (err) {
-            ReadError.EndOfStream => break
-        }
+    while (buf_read.streamUntilDelimiter(a.writer(), '\n', 40)): (a.shrinkRetainingCapacity(0)) {
+        var iter = splitSequence(u8, a.items, " ");
+        const foo = iter.next().?;
+        const num: i32 = try std.fmt.parseInt(i32, foo, 10);
+        dbgPrt("{s} | {d}\n", .{ a.items, num });
+    } else |_| {
+        dbgPrt("Reached the end of input", .{});
     }
 
 
