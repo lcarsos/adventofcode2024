c367108786b2130ea3e8c9e2f091a0cb3483645f
lcarsos <lcarsos@lcarsos.com>
2025-06-12 11:38:47 -0600

    finally figured out how I could walk off the grid

--- 04/src/Program.cs
+++ 04/src/Program.cs
@@ -15,13 +15,96 @@ namespace Advent {
         }
     }
 
+    public enum Direction {
+        N,
+        NE,
+        E,
+        SE,
+        S,
+        SW,
+        W,
+        NW
+    }
+
+    public record Point {
+        public int x { get; init; }
+        public int y { get; init; }
+
+        public sealed override string ToString() => $"({x}, {y})";
+
+        public Point Move(Direction dir) =>
+            dir switch
+            {
+                Direction.N => new Point { x = x - 1, y = y },
+                Direction.NE => new Point { x = x - 1, y = y + 1 },
+                Direction.E => new Point { x = x, y = y + 1 },
+                Direction.SE => new Point { x = x + 1, y = y + 1 },
+                Direction.S => new Point { x = x + 1, y = y },
+                Direction.SW => new Point { x = x + 1, y = y - 1 },
+                Direction.W => new Point { x = x, y = y - 1 },
+                Direction.NW => new Point { x = x - 1, y = y - 1 },
+                _ => throw new ArgumentException("Foobar", nameof(dir)),
+        };
+    }
+
     public class Grid {
         private readonly string[] m_grid;
+        public int columns { get; }
+        public int rows { get; }
 
         public Grid(string[] grid) {
             m_grid = grid;
+            rows = m_grid.Length;
+            columns = m_grid[0].Length;
         }
 
-        
+        public char? Cell(int x, int y) {
+            if (x < 0 || x >= rows) return null;
+            if (y < 0 || y >= columns) return null;
+
+            return m_grid[x][y];
+        }
+
+        public char? Cell(Point pt) {
+            return Cell(pt.x, pt.y);
+        }
+
+        public char?[] Neighbors(int x, int y) {
+            char?[] neighbors = new char?[8];
+            neighbors[(int)Direction.N] ??= Cell(x - 1, y);
+            neighbors[(int)Direction.NE] ??= Cell(x - 1, y + 1);
+            neighbors[(int)Direction.E] ??= Cell(x, y + 1);
+            neighbors[(int)Direction.SE] ??= Cell(x + 1, y + 1);
+            neighbors[(int)Direction.S] ??= Cell(x + 1, y);
+            neighbors[(int)Direction.SW] ??= Cell(x + 1, y - 1);
+            neighbors[(int)Direction.W] ??= Cell(x, y - 1);
+            neighbors[(int)Direction.NW] ??= Cell(x - 1, y - 1);
+            return neighbors;
+        }
+
+        public bool DoesMatch(Point start, Direction heading, string match) {
+            Point stand = start;
+            for (int i = 0; i < match.Length; i++) {
+                if (Cell(stand) == null || Cell(stand) != match[i]) {
+                    return false;
+                }
+                stand = stand.Move(heading);
+            }
+            return true;
+        }
+
+        public int CountMatches(string toCheck) {
+            int count = 0;
+            for (int i = 0; i < grid.rows; i++) {
+                for (int j = 0; j < grid.columns; j++) {
+                    foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
+                        Point pt = new Point { x = i, y = j };
+                        if (grid.DoesMatch(pt, dir, toCheck)) {
+                            count += 1;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- 04/test/UnitTest1.cs
+++ 04/test/UnitTest1.cs
@@ -2,16 +2,119 @@ using Advent;
 
 namespace test;
 
-public class UnitTest1
+public class AdventGridTests
 {
+    private readonly ITestOutputHelper logger;
+
+    public AdventGridTests(ITestOutputHelper logger) {
+        this.logger = logger;
+    }
+
     [Fact]
-    public void Test1()
+    public void TestNeighbors()
     {
-        Grid grid = Grid([
+        Grid grid = new Grid([
             "abcde",
             "fghij",
             "klmno",
         ]);
-        Assert.Equal([], grid.Neighbors(1, 2));
+        Assert.Equal(['c', 'd', 'i', 'n', 'm', 'l', 'g', 'b'], grid.Neighbors(1, 2));
+        Assert.Equal(['f', 'g', 'l', null, null, null, null, null], grid.Neighbors(2, 0));
+    }
+
+    [Fact]
+    public void TestMovingPoints()
+    {
+        Point pt = new Point { x = 2, y = 2 };
+        Point newPt = pt.Move(Direction.N);
+
+        Assert.Equal(new Point { x = 1, y = 2 }, newPt);
+
+        Grid grid = new Grid([
+            ".Sl..",
+            "deA..",
+            "...M.",
+            "....X",
+        ]);
+        Point stand = new Point { x = 0, y = 0 };
+        stand = stand.Move(Direction.E);
+        Assert.Equal(grid.Cell(stand), 'S');
+        stand = stand.Move(Direction.E);
+        Assert.Equal(grid.Cell(stand), 'l');
+        stand = stand.Move(Direction.S);
+        Assert.Equal(grid.Cell(stand), 'A');
+    }
+
+    [Fact]
+    public void CheckWalkForwardTest()
+    {
+        string toCheck = "XMAS";
+        Grid grid = new Grid([
+            ".XMAS..",
+        ]);
+
+        Assert.True(grid.DoesMatch(new Point { x = 0, y = 1 }, Direction.E, toCheck));
+        Assert.False(grid.DoesMatch(new Point { x = 0, y = 1 }, Direction.N, toCheck));
+        Assert.False(grid.DoesMatch(new Point { x = 0, y = 0 }, Direction.E, toCheck));
+        Assert.False(grid.DoesMatch(new Point { x = 0, y = 2 }, Direction.E, toCheck));
+        Assert.False(grid.DoesMatch(new Point { x = 0, y = 1 }, Direction.W, toCheck));
+
+        grid = new Grid([
+            ".SA..",
+            "deAM.",
+            "...MX",
+            "....X",
+        ]);
+        Assert.True(grid.DoesMatch(new Point { x = 3, y = 4 }, Direction.NW, toCheck));
+        Assert.False(grid.DoesMatch(new Point { x = 2, y = 3 }, Direction.NW, toCheck));
+    }
+
+    [Fact]
+    public void TestMoving()
+    {
+        Grid grid = new Grid([
+            "abc",
+            "fgh",
+            "klm",
+        ]);
+        Point stand = new Point { x = 1, y = 1 };
+
+        Assert.Equal(grid.Cell(stand.Move(Direction.N)), 'b');
+        Assert.Equal(grid.Cell(stand.Move(Direction.E)), 'h');
+        Assert.Equal(grid.Cell(stand.Move(Direction.S)), 'l');
+        Assert.Equal(grid.Cell(stand.Move(Direction.W)), 'f');
+
+        Assert.Equal(grid.Cell(stand.Move(Direction.NW)), 'a');
+    }
+
+    [Fact]
+    public void CountInputStringsTest()
+    {
+        string toCheck = "XMAS";
+        Grid grid = new Grid([
+            "MMMSXXMASM",
+            "MSAMXMSMSA",
+            "AMXSXMAAMM",
+            "MSAMASMSMX",
+            "XMASAMXAMM",
+            "XXAMMXXAMA",
+            "SMSMSASXSS",
+            "SAXAMASAAA",
+            "MAMMMXMMMM",
+            "MXMXAXMASX",
+        ]);
+
+        int count = 0;
+        for (int i = 0; i < grid.rows; i++) {
+            for (int j = 0; j < grid.columns; j++) {
+                foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
+                    Point pt = new Point { x = i, y = j };
+                    if (grid.DoesMatch(pt, dir, toCheck)) {
+                        count += 1;
+                    }
+                }
+            }
+        }
+        Assert.Equal(18, count);
     }
 }
