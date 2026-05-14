26c90fc663a375c0e21ce712ca6318bbbdfb0f02
= <lcarsos@lcarsos.com>
2026-05-13 19:17:31 -0600

    relearning how buffers work

--- 06/part_one.zig
+++ 06/part_one.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
+const Dir = std.Io.Dir;
 const Allocator = std.mem.Allocator;
-const io = @import("io");
+//const io = @import("io");
 
 const PointArray = std.ArrayList(Point);
 
@@ -37,18 +38,25 @@ pub fn load_map(alloc: Allocator) .{PointArray, Point} {
 }
 
 pub fn main(init: std.process.Init) !void {
-    const pwd = init.environ_map.get("CWD") orelse "N/A";
     const count = init.minimal.args.vector.len;
-    const arg = init.minimal.args.vector[1];
+    const arg: []const u8 = std.mem.span(init.minimal.args.vector[1]);
+    const io = init.io;
+
+    var buffer: [1024]u8 = undefined;
+    const buf_slice: []u8 = buffer[0..1024];
+
+    const file_ref = Dir.openFile(Dir.cwd(), io, arg, .{ .mode = Dir.OpenFileOptions.Mode.read_only }) catch {
+        std.debug.print("failed to open file {s}\n", .{arg,});
+        return;
+    };
+    const length = try file_ref.readPositionalAll(io, buf_slice, 0);
+    const content = buffer[0..length];
 
 
-    const file_ref = init.io.vtable.file
-
-
-
-    std.debug.print("hello world {s}\n", .{pwd,});
     std.debug.print("num args {d}\n", .{count,});
     std.debug.print("arg 1 {s}\n", .{arg,});
+    std.debug.print("read {d} bytes\n", .{length,});
+    std.debug.print("{c}\n", .{content[4],});
 }
 
 test "test input" {
